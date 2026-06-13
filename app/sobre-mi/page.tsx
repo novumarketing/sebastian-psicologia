0c79ec476208cd61e14e2f6b16dfdb573f5ea7e3
novumarketing <novumarketing03@gmail.com>
2026-06-13 09:45:28 -0600

    Add files via upload

--- app/sobre-mi/page.tsx
+++ app/sobre-mi/page.tsx
@@ -3,8 +3,8 @@ import About from '@/components/About'
 import Footer from '@/components/Footer'
 
 export const metadata = {
-  title: 'Sobre mí | Mtro. Sebastián Escorza',
-  description: 'Conoce al Mtro. C. Sebastián Escorza Reyes, psicólogo especializado en ACC.',
+  title: 'Sobre mí | Psic. Sebastián Escorza',
+  description: 'Conoce al Psic. C. Sebastián Escorza Reyes, psicólogo especializado en ACC.',
 }
 
 export default function SobreMiPage() {
