e47d2723c3134c29bd1650bcd6240de66e7f2e81
novumarketing <novumarketing03@gmail.com>
2026-06-13 00:55:53 -0600

    refactor: multi-page app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Services from '@/components/Services'
-import TCC from '@/components/TCC'
-import Locations from '@/components/Locations'
-import Appointments from '@/components/Appointments'
 import Footer from '@/components/Footer'
 
 export default function Home() {
@@ -13,11 +8,6 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <About />
-        <Services />
-        <TCC />
-        <Locations />
-        <Appointments />
       </main>
       <Footer />
     </>
